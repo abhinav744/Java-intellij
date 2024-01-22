e688ab2cc9fee752201b24a84bf4187116a743dd
HP <abhinavchauhan744@gmail.com>
2024-01-22 12:54:30 +0530

    Second commit

--- GenDemo.java
+++ GenDemo.java
@@ -23,11 +23,11 @@ void showType() {
 class GenDemo {
     public static void main(String args[]) {
 // Create a Gen reference for Integers.
-        Gen<Integer> iOb;
+        Gen2<Integer> iOb;
 // Create a Gen<Integer> object and assign its
 // reference to iOb. Notice the use of autoboxing
 // to encapsulate the value 88 within an Integer object.
-        iOb = new Gen<Integer>(88);
+        iOb = new Gen2<Integer>(88);
 // Show the type of data used by iOb.
         iOb.showType();
 // Get the value in iOb. Notice that
@@ -36,7 +36,7 @@ class GenDemo {
         System.out.println("value: " + v);
         System.out.println();
 // Create a Gen object for Strings.
-        Gen<String> strOb = new Gen<String> ("Generics Test");
+        Gen2<String> strOb = new Gen2<String>("Generics Test");
 // Show the type of data used by strOb.
         strOb.showType();
 // Get the value of strOb. Again, notice
